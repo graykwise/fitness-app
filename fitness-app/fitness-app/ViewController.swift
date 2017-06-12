591e41f6589e29e8ecbba433c556655aba7ea986
Grayson Wise <graysonwise3@gmail.com>
2017-06-12 21:49:02 +0200

    Initial commit with the newly created xcode file

---
+++ fitness-app/fitness-app/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  fitness-app
+//
+//  Created by Grayson Wise on 6/12/17.
+//  Copyright © 2017 Dubs Dev. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
