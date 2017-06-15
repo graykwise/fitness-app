295b0cf86375ecddfa6c85f6ac766e8ae89ff51b
Grayson Wise <graysonwise3@gmail.com>
2017-06-15 21:56:38 +0200

    Most up to date code

--- fitness-app/fitness-app/JoinViewController.swift
+++ fitness-app/fitness-app/JoinViewController.swift
@@ -14,6 +14,7 @@ class JoinViewController: UIViewController, UITableViewDelegate, UITableViewData
     @IBOutlet weak var leaguesTable: UITableView!
     
     var leagues = Array<League>()
+    var selectedLeague = League()
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -35,7 +36,7 @@ class JoinViewController: UIViewController, UITableViewDelegate, UITableViewData
                     let league = League()
                     
                     if let leagueDictionary = value as? [String: AnyObject] {
-                        league.myName = leagueDictionary["Name"] as! String
+                        league.myName = leagueDictionary["name"] as! String
                         self.leagues.append(league)
                     }
                 }
@@ -58,7 +59,8 @@ class JoinViewController: UIViewController, UITableViewDelegate, UITableViewData
     }
     
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        
+        selectedLeague = leagues[indexPath.item]
+
     }
     
     func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
@@ -74,6 +76,10 @@ class JoinViewController: UIViewController, UITableViewDelegate, UITableViewData
     
     @IBAction func backToChoose(_ sender: UIBarButtonItem) {
         dismiss(animated: true, completion: nil)
+    }
+    
+    @IBAction func joinLeague(_ sender: UIButton) {
+        
     }
     
 
